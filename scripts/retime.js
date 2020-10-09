39942d96b1ca23a19fc6fb8081bc33307d69296b
Rudigus <rodrigo.matos9@hotmail.com>
2020-10-09 00:41:11 -0300

    still ugly, but better

--- scripts/retime.js
+++ scripts/retime.js
@@ -13,5 +13,5 @@ function calculate() {
     let minutes = Math.floor(runTime / 60) - hours * 60
     let seconds = Math.floor(runTime) - hours * 3600 - minutes * 60
     let milliseconds = Math.round((runTime * 1000 - (hours * 3600 + minutes * 60 + seconds) * 1000) * 1000) / 1000
-    runTimeParagraph.textContent = `Run Time: ${hours}h ${minutes}m ${seconds}s ${milliseconds}ms`
+    runTimeParagraph.innerHTML = `Run Time<br>${hours}h ${minutes}m ${seconds}s ${milliseconds}ms`
 }
